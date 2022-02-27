6bcb653fc3c66091a857c83c9290e4e94b37fe8f
niranjannlc <niranjannlc10@gmail.com>
2022-02-27 20:29:57 +0545

    test cases added

---
+++ app/src/androidTest/java/com/example/reterofitmvvm/modal/repo/UniversityRepoTest.kt
@@ -0,0 +1,13 @@
+package com.example.reterofitmvvm.modal.repo
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class UniversityRepoTest : TestCase()
+{
+    @Test
+    fun testSimplFunction()
+    {
+        assertNull(null)
+    }
+}
